8f45cc2fc397a39039c270be32494b519f2cfe5a
Chintan Sawla <59383889+sawlachintan@users.noreply.github.com>
2023-04-10 14:57:19 -0400

    add runs per wicket
    also database contains teams of IPL 2023

--- pages/team.py
+++ pages/team.py
@@ -23,12 +23,16 @@ ABB = {
     "lsg": ['Lucknow Super Giants', "#30D5C8"],
 }
 
+TEAMS = [val[0] for key, val in ABB.items()]
+
 plotly_tempate = pio.templates['plotly_dark']
 URL = 'https://sawlachintan.github.io/cricket_data/ipl_data/'
 innings_df = pd.read_csv(f'{URL}innings_df.csv')
 info_df = pd.read_csv(f'{URL}info_df.csv')
 merge_df = innings_df.merge(info_df[['key', 'date']], on='key')
 merge_df['date'] = pd.to_datetime(merge_df['date']).dt.year
+merge_df = merge_df.loc[merge_df.batting_team.isin(
+    TEAMS), :].reset_index(drop=True)
 
 
 def test_fn(x: str) -> html.Button: return html.Button(x.upper(), id=f'{x}-btn', n_clicks=0,
@@ -41,16 +45,17 @@ layout = html.Div(children=[html.Div(children=[
     #         className='text-2xl font-medium'),
     html.Div(children=[test_fn(x) for x in ABB],
              className='col-span-3 overflow-auto flex gap-2 justify-between'),
-    dcc.RangeSlider(2008, 2022, 1, value=[2016, 2022],
+    dcc.RangeSlider(2008, 2023, 1, value=[2016, 2023],
                     id='my-range-slider',
-                    marks={i: f'{i}' for i in range(2008, 2023)}, className='col-span-2', persistence='years')
+                    marks={i: f'{i}' for i in range(2008, 2024)}, className='col-span-2', persistence='years')
 
 ], className='dark:text-white flex flex-col gap-5 md:grid md:grid-cols-5 md:gap-4'),
     html.Div(children=[
-        html.H1('dfkjdlsjflk', className='dark:text-white font-extrabold text-3xl'),
+
 
         dcc.Graph(id='runs-line', config={'displaylogo': False}),
-        dcc.Graph(id='bndry-vs-dots', config={'displaylogo': False})
+        dcc.Graph(id='bndry-vs-dots', config={'displaylogo': False}),
+        dcc.Graph(id='runs-per-wick', config={'displaylogo': False})
     ])], className='px-5 flex flex-col gap-3')
 
 
@@ -86,7 +91,6 @@ def on_click(all_btns: dict, all_states: dict) -> dict:
 
     all_class[team_trig] = f'{all_class[team_trig]} bg-{team_trig}'
 
-
     return {'all_class': all_class}
 
 
@@ -105,8 +109,6 @@ def runs_graph(all_btns, years):
 
     team_trig = ABB[find_key(c)][0]
     color = ABB[find_key(c)][1]
-
-
 
     all_runs = merge_df[(merge_df.date >= years[0]) & (merge_df.date <= years[1])]\
         .groupby(['date'])\
@@ -156,16 +158,12 @@ def boundary_over_dots(data: pd.DataFrame,
         data = data[(data['date'] >= period[0]) & (data['date'] <= period[1])]\
             .copy(deep=True)\
             .reset_index(drop=True)
-        
-
 
     # group by over and runs. Get count of 0, 4 and 6 in each over
     agg_data = data.groupby(['over', 'runs_batter']).agg(
         {'key': 'count'}).reset_index()
     agg_data = agg_data[(agg_data.runs_batter == 0) | (
         agg_data.runs_batter == 4) | (agg_data.runs_batter == 6)]
-    
-
 
     # sum 4 and 6 as one metric
     agg_data['d_b'] = agg_data.groupby(['over', agg_data['runs_batter'] == 0])[
@@ -213,8 +211,6 @@ def bndry_dots(all_btns, years):
     all_bndrs = boundary_over_dots(merge_df, period=years)
     for_team = boundary_over_dots(merge_df, team_trig, years)
 
-
-
     everyone_col = ['#ffffff']*20
     colors = [color]*20
 
@@ -224,7 +220,60 @@ def bndry_dots(all_btns, years):
         go.Bar(x=for_team['over'], y=for_team['b_over_d'], name=team_trig, marker_color=colors))
     fig.add_trace(go.Bar(x=all_bndrs['over'], y=all_bndrs['b_over_d'],
                   name='All Teams', marker_color=everyone_col, opacity=0.15))
+    fig.add_vline(x=5.5, col='#666666', line_dash="dash", line_color="grey")
+    fig.add_vline(x=15.5, col='#666666', line_dash="dash", line_color="grey")
     fig.update_layout(title='Ratio of boundaries over dots across the innings',
                       xaxis_title='Over', yaxis_title='Ratio of boundaries over dots', template=plotly_tempate)
 
+    return fig
+
+
+def rpw_df(data: pd.DataFrame,
+           period: Optional[List[int]] = [2008, 2022]) -> pd.DataFrame:
+    # if team:
+    #     data = data[data.batting_team == team]\
+    #         .copy(deep=True)\
+    #         .reset_index(drop=True)
+    if period:
+        period = sorted(period)
+        data = data[(data['date'] >= period[0]) & (data['date'] <= period[1])]\
+            .copy(deep=True)\
+            .reset_index(drop=True)
+
+    agg_data = data.groupby('batting_team').agg(
+        {'runs_total': 'sum', 'wicket_kind': 'count'}).reset_index()
+    agg_data['runs_per_wicket'] = round(
+        agg_data['runs_total'] / agg_data['wicket_kind'], 2)
+    return agg_data
+
+
+@callback(Output('runs-per-wick', 'figure'), inputs={'all_btns': {
+    team: Input(f'{team}-btn', 'n_clicks') for team in ABB
+}, 'years': Input('my-range-slider', 'value')})
+def rpw_plot(all_btns, years):
+    def find_key(all_btns: dict) -> str:
+        for key in all_btns:
+            if all_btns[key].triggered:
+                return key
+        return 'csk'
+
+    c = dash.ctx.args_grouping.all_btns  # type: ignore
+
+    team_trig = ABB[find_key(c)][0]
+    color = ABB[find_key(c)][1]
+
+    rpw = rpw_df(merge_df, years)
+    all_time = round(sum(rpw['runs_total']) / sum(rpw['wicket_kind']), 2)
+    layout = go.Layout(barmode='overlay')
+    rpw['color'] = '#666666'
+    rpw.loc[rpw.batting_team == team_trig, 'color'] = color
+    fig = go.Figure(layout=layout)
+
+    fig.add_trace(
+        go.Bar(x=rpw['batting_team'], y=rpw['runs_per_wicket'], marker_color=rpw['color']))
+
+    fig.add_hline(y=all_time, line_dash="dot")
+
+    fig.update_layout(title='Runs per wicket',
+                      xaxis_title='Teams', yaxis_title='Runs per Wicket', template=plotly_tempate)
     return fig
